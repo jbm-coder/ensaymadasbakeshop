00a5270ffb61afa953380106aa62deb34d8c5adf
jbm-coder <115842129+jbm-coder@users.noreply.github.com>
2023-07-13 15:39:13 +1000

    setting up the products data, pictures and styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,25 @@
+import data from './data';
+
 function App() {
   return (
     <div>
       <header>
         <a href="/">ensaymadas</a>
       </header>
-      <main>list products</main>
+      <main>
+        <h1>Welcome to Ensaymadas Bake Shop</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <img src={product.image} alt={product.name} />
+              <div class="product-info">
+                <p>{product.name}</p>
+                <p>{product.price}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
---
+++ frontend/src/data.js
@@ -0,0 +1,41 @@
+const data = {
+  products: [
+    {
+      name: 'Ensaymada',
+      slug: 'ensaymada',
+      image: '/images/pic1.jpg', // 180px x 180px
+      price: 3.5,
+      rating: 4.5,
+      numReviews: 10,
+      description: 'Freshly baked ensaymada',
+    },
+    {
+      name: 'Ube Ensaymada',
+      slug: 'ube-ensaymada',
+      image: '/images/pic2.jpg', // 180px x 180px
+      price: 4.0,
+      rating: 4.5,
+      numReviews: 10,
+      description: 'Freshly baked ensaymada',
+    },
+    {
+      name: 'Ube Halaya',
+      slug: 'ube-halaya',
+      image: '/images/pic3.jpg', // 180px x 180px
+      price: 8.0,
+      rating: 4.5,
+      numReviews: 10,
+      description: 'Freshly baked ensaymada',
+    },
+    {
+      name: 'Cassava Cake',
+      slug: 'cassava-cake',
+      image: '/images/pic4.jpg', // 180px x 180px
+      price: 10.0,
+      rating: 4.5,
+      numReviews: 10,
+      description: 'Freshly baked ensaymada',
+    },
+  ],
+};
+export default data;
