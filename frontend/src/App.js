7e00f1dfa6904acd222137176b0ec3269ccc7fe4
jbm-coder <115842129+jbm-coder@users.noreply.github.com>
2023-07-13 12:18:22 +1000

    React App front end template

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">ensaymadas</a>
+      </header>
+      <main>list products</main>
+    </div>
+  );
+}
+
+export default App;
